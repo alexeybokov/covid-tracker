33c610391d4d3a5ea7d2b772ccaf4c8e3af13831
alexeybokov <lexabokov18@gmail.com>
2020-05-04 15:31:29 +0300

    update styles, add chart to packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./services/covid-api-service";
+import styles from "./App.module.css";
+
+import coronaImage from "./images/corona.png";
 
 export default class App extends Component {
+
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  }
+
   render() {
+    const { data, country } = this.state;
+
     return (
-      <div>
-        <h1>Covid  Info</h1>
-        <Cards />
-        <Chart />
-        <CountryPicker />
+      <div className={styles.container} >
+        <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+        <Cards data={data}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country}/>
       </div>
     );
   }

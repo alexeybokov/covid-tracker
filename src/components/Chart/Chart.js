82ea8f74f707bc0d590abf55460edffe1bce0f83
alexeybokov <lexabokov18@gmail.com>
2020-05-05 16:06:45 +0300

    add active to country statistic

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,7 @@ import { Line, Bar } from "react-chartjs-2";
 import { fetchDailyData, fetchDailyDelta } from "../../services/covid-api-service";
 import styles from "./Chart.module.css";
 
-const Charts = ({ data: { confirmed, recovered, deaths, active }, country, chartOption }) => {
+const Charts = ({ data: { confirmed, recovered, deaths }, country, chartOption }) => {
   const [dailyData, setDailyData] = useState([]);
   const [dailyDelta, setDailyDelta] = useState([]);
 
@@ -43,19 +43,20 @@ const Charts = ({ data: { confirmed, recovered, deaths, active }, country, chart
       ? (
         <Bar
           data={{
-            labels: ['Infected', 'Recovered', 'Deaths'],
+            labels: ['Infected', 'Recovered', 'Deaths', 'Active'],
             datasets: [{
               label: 'People',
               backgroundColor: [
                 'rgba(0, 0, 255, 0.5)',
                 'rgba(0, 255, 0, 0.5)',
                 'rgba(255, 0, 0, 0.5)',
-                // 'rgba(255, 0, 0, 0.5)'
+                'rgb(227,86,21)'
               ],
               data: [
                 confirmed.value,
                 recovered.value,
-                deaths.value]
+                deaths.value,
+                confirmed.value - recovered.value - deaths.value]
             }]
 
         }}
